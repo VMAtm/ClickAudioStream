29835d71d9578d1ec358a1421dd8693389a5e8e0
Eugene Batalov <eabatalov89@gmail.com>
2015-11-18 23:21:59 +0300

    see the changes description in commit description
    - Changed files structure. Separated src and other resources.
    - Simplified nodejs server. Now it simply serves static content.
      We should enforce client to adjust download speed and play music
      as stream no intervention from server side should be provided.
      Audio is played in stream mode on my chrome after this change.
    - Added autoplay=true to Audio objects. This forces browser
      to play as soon as enough data buffer is fetched.

--- src/scripts/Scheduler.js
+++ src/scripts/Scheduler.js
@@ -1,9 +1,10 @@
 function Scheduler(s) {
   this.schedule = s;
   this.schedule.forEach(function onProcess(current, index, array) {
-    this.track = new Audio('?file=' + current.name);
+    this.track = new Audio('/res/audio/' + current.name);
     this.track.currentTime = current.seekTo;
     this.track.loop = current.loop;
+    this.track.preload = 'auto';
     this.startAfter = current.startAfter;
   });
 }
@@ -11,6 +12,7 @@ function Scheduler(s) {
 Scheduler.prototype = {
   play() {
     setTimeout(function () {
+      this.track.autoplay = true;
       this.track.play();
     }, this.startAfter ? this.startAfter : 0);
   }
